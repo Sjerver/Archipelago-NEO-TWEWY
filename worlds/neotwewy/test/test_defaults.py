3222a4d533a0b1010874f3dbe5fade2f6ff06028
Sjerver <sjerverino@gmail.com>
2026-07-26 17:39:32 +0200

    Fixes Tests

--- worlds/neotwewy/test/test_defaults.py
+++ worlds/neotwewy/test/test_defaults.py
@@ -6,5 +6,5 @@ class TestDefault(NEOTwewyTestBase):
         "minamimoto_pin": False
     }
 
-def test_no_minamimoto_starting_pin(self):
-    self.assertRaises(KeyError, self.world.get_location, "W1D1 - Minamimoto's Pin")
+    def test_no_minamimoto_starting_pin(self):
+        self.assertRaises(KeyError, self.world.get_location, "W1D1 - Minamimoto's Pin")
--- worlds/neotwewy/test/test_starting_pin.py
+++ worlds/neotwewy/test/test_starting_pin.py
@@ -6,19 +6,19 @@ class TestStartingPin(NEOTwewyTestBase):
         "minamimoto_pin": True
     }
 
-def test_day_entrance_conditions(self):
-    with self.subTest("Test reaching W1D2"):
-        self.collect_by_name("Secret Report No. 1")
-        self.assertTrue(self.can_reach_region("W1D2"))
-    with self.subTest("Test reaching W1D3"):
-        self.collect_by_name("Secret Report No. 1")
-        self.assertFalse(self.can_reach_region("W1D3"))
+    def test_day_entrance_conditions(self) -> None:
+        with self.subTest("Test reaching W1D2"):
+            self.collect_by_name("Secret Report No. 1")
+            self.assertTrue(self.can_reach_region("W1D2"))
+        with self.subTest("Test reaching W1D3"):
+            self.collect_by_name("Secret Report No. 1")
+            self.assertFalse(self.can_reach_region("W1D3"))
 
-        self.collect_by_name("Secret Report No. 2")
-        self.assertTrue(self.can_reach_region("W1D3"))
+            self.collect_by_name("Secret Report No. 2")
+            self.assertTrue(self.can_reach_region("W1D3"))
 
-def test_minamimoto_starting_pin(self):
-    try:
-        self.world.get_location("W1D1 - Minamimoto's Pin")
-    except KeyError:
-        self.fail("Minamimoto's Pin should exist, but it doesn't.")
+    def test_minamimoto_starting_pin(self) -> None:
+        try:
+            self.world.get_location("W1D1 - Minamimoto's Pin")
+        except KeyError:
+            self.fail("Minamimoto's Pin should exist, but it doesn't.")
